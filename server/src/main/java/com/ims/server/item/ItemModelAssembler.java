5e099889e24066366a6e60dba166f8a3ce72aa19
mihiran-paranamana <mihiran.hlrm@gmail.com>
2021-05-01 11:15:29 +0530

    modifing response rels

--- server/src/main/java/com/ims/server/item/ItemModelAssembler.java
+++ server/src/main/java/com/ims/server/item/ItemModelAssembler.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import com.ims.server.itemAction.ItemActionController;
+
 @Component
 public class ItemModelAssembler implements RepresentationModelAssembler<Item, EntityModel<Item>> {
 
@@ -13,7 +15,8 @@ public class ItemModelAssembler implements RepresentationModelAssembler<Item, En
         return EntityModel.of(
                 item,
                 linkTo(methodOn(ItemController.class).getItem(item.getId())).withSelfRel(),
-                linkTo(methodOn(ItemController.class).getItems()).withRel("items")
+                linkTo(methodOn(ItemController.class).getItems()).withRel("items"),
+                linkTo(methodOn(ItemActionController.class).getItemActions(item.getId())).withRel("itemActions")
         );
     }
 }
--- server/src/main/java/com/ims/server/itemAction/ItemActionModelAssembler.java
+++ server/src/main/java/com/ims/server/itemAction/ItemActionModelAssembler.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+import com.ims.server.item.ItemController;
+
 @Component
 public class ItemActionModelAssembler implements RepresentationModelAssembler<ItemAction, EntityModel<ItemAction>> {
 
@@ -13,6 +15,7 @@ public class ItemActionModelAssembler implements RepresentationModelAssembler<It
         return EntityModel.of(
                 itemAction,
                 linkTo(methodOn(ItemActionController.class).getItemAction(itemAction.getItem().getId(), itemAction.getId())).withSelfRel(),
+                linkTo(methodOn(ItemController.class).getItems()).withRel("items"),
                 linkTo(methodOn(ItemActionController.class).getItemActions(itemAction.getItem().getId())).withRel("itemActions")
         );
     }
