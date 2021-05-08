ec268175d2bffc35a784f42c25bddd2b3348b1e5
mihiran-paranamana <mihiran.hlrm@gmail.com>
2021-05-09 05:06:13 +0530

    update server timezone

--- server/src/main/java/com/ims/server/ServerApplication.java
+++ server/src/main/java/com/ims/server/ServerApplication.java
@@ -3,6 +3,8 @@ package com.ims.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,4 +14,8 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Colombo"));
+	}
 }
